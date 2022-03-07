c7e482e2473308c0d8a8dcffeafd987026f079a5
Chris Sweeney <93913592+ChrisSweeneyIRIS@users.noreply.github.com>
2022-03-07 14:31:21 +0000

    Ex2 complete

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,19 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data import session_items
+from flask import Flask, render_template, redirect, url_for, request
 
+from todo_app.data import trello_items as trello
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
-app.config.from_object(Config())
+app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
-    items = session_items.get_items()
+    items = trello.get_items()
     return render_template("index.html", items = items)
 
 @app.route('/items/new', methods=['POST'])
 def add_item():
     title = request.form['title']
-    session_items.add_item(title)
+    trello.add_item(title)
     return redirect(url_for('index'))
+
+@app.route('/items/<id>/complete')
+def complete_item(id):
+    trello.complete_item(id)
+    return redirect(url_for('index'))
+
+@app.route('/items/<id>/start')
+def start_item(id):
+    trello.start_item(id)
+    return redirect(url_for('index')) 
+
+@app.route('/items/<id>/start')
+def start_item(id):
+    trello.start_item(id)
+    return redirect(url_for('index')) 
+
+if __name__ == '__main__':
+    app.run()
--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -2,8 +2,9 @@ import os
 
 
 class Config:
-    def __init__(self):
-        """Base configuration variables."""
-        self.SECRET_KEY = os.environ.get('SECRET_KEY')
-        if not self.SECRET_KEY:
-            raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
+    # Trello API Details
+    TRELLO_BASE_URL = "https://api.trello.com/1"
+    TRELLO_API_KEY = os.environ.get("TRELLO_API_KEY")
+    TRELLO_API_SECRET = os.environ.get("TRELLO_API_SECRET")
+    TRELLO_BOARD_ID = os.environ.get("TRELLO_BOARD_ID")
+    TRELLO_USERNAME = os.environ.get("TRELLO_USERNAME")
